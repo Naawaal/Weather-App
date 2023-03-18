8083ef6a42b3af4c820b0d5bfb0f5dbcc673d46a
Nawal2525 <karanstha197@gmail.com>
2023-03-18 15:22:47 +0545

    change on ui

--- lib/views/homepage_screen.dart
+++ lib/views/homepage_screen.dart
@@ -27,97 +27,159 @@ class HomepageScreen extends StatelessWidget {
       ),
       body: Container(
         padding: const EdgeInsets.all(12),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            "Kathmandu".text.bold.size(30).letterSpacing(3).make(),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Image.asset(
-                  "assets/weather/01d.png",
-                  width: 60,
-                ),
-                const SizedBox(
-                  width: 10,
-                ),
-                RichText(
-                  text: const TextSpan(
-                    children: [
-                      TextSpan(
-                        text: "30\u00B0 ",
-                        style: TextStyle(
-                          color: Vx.gray900,
-                          fontSize: 30,
+        child: SingleChildScrollView(
+          physics: const BouncingScrollPhysics(),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              "Kathmandu".text.bold.size(30).letterSpacing(3).make(),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Image.asset(
+                    "assets/weather/01d.png",
+                    width: 60,
+                  ),
+                  const SizedBox(
+                    width: 10,
+                  ),
+                  RichText(
+                    text: const TextSpan(
+                      children: [
+                        TextSpan(
+                          text: "30\u00B0 ",
+                          style: TextStyle(
+                            color: Vx.gray900,
+                            fontSize: 30,
+                          ),
                         ),
-                      ),
-                      TextSpan(
-                        text: "Sunny",
-                        style: TextStyle(
-                          color: Vx.gray900,
-                          letterSpacing: 3,
-                          fontSize: 14,
+                        TextSpan(
+                          text: "Sunny",
+                          style: TextStyle(
+                            color: Vx.gray900,
+                            letterSpacing: 3,
+                            fontSize: 14,
+                          ),
                         ),
-                      ),
-                    ],
-                  ),
-                ),
-              ],
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.end,
-              children: [
-                TextButton.icon(
-                  onPressed: () {},
-                  icon: const Icon(
-                    Icons.expand_less_outlined,
-                    color: Vx.gray400,
-                  ),
-                  label: "14\u00B0 ".text.make(),
-                ),
-                TextButton.icon(
-                  onPressed: () {},
-                  icon: const Icon(
-                    Icons.expand_more_outlined,
-                    color: Vx.gray400,
-                  ),
-                  label: "20\u00B0 ".text.make(),
-                ),
-              ],
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
-              children: List.generate(3, (index) {
-                var values = [
-                  "70%",
-                  "40%",
-                  "3.5 km/h",
-                ];
-                var iconList = [
-                  clouds,
-                  humidity,
-                  windspeed,
-                ];
-                return Column(
-                  children: [
-                    Image.asset(
-                      iconList[index],
-                      width: 50,
-                    )
-                        .box
-                        .gray200
-                        .padding(const EdgeInsets.all(8))
-                        .roundedSM
-                        .make(),
-                    const SizedBox(
-                      height: 5,
+                      ],
                     ),
-                    values[index].text.make()
-                  ],
-                );
-              }),
-            ),
-          ],
+                  ),
+                ],
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  TextButton.icon(
+                    onPressed: () {},
+                    icon: const Icon(
+                      Icons.expand_less_outlined,
+                      color: Vx.gray400,
+                    ),
+                    label: "14\u00B0 ".text.make(),
+                  ),
+                  TextButton.icon(
+                    onPressed: () {},
+                    icon: const Icon(
+                      Icons.expand_more_outlined,
+                      color: Vx.gray400,
+                    ),
+                    label: "20\u00B0 ".text.make(),
+                  ),
+                ],
+              ),
+              10.heightBox,
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: List.generate(3, (index) {
+                  var values = [
+                    "70%",
+                    "40%",
+                    "3.5 km/h",
+                  ];
+                  var iconList = [
+                    clouds,
+                    humidity,
+                    windspeed,
+                  ];
+                  return Column(
+                    children: [
+                      Image.asset(
+                        iconList[index],
+                        width: 50,
+                      )
+                          .box
+                          .gray200
+                          .padding(const EdgeInsets.all(8))
+                          .roundedSM
+                          .make(),
+                      10.heightBox,
+                      values[index].text.gray400.make()
+                    ],
+                  );
+                }),
+              ),
+              10.heightBox,
+              const Divider(
+                color: Vx.gray700,
+              ),
+              10.heightBox,
+              SizedBox(
+                height: 160,
+                child: ListView.builder(
+                  scrollDirection: Axis.horizontal,
+                  physics: const BouncingScrollPhysics(),
+                  shrinkWrap: true,
+                  itemCount: 6,
+                  itemBuilder: (context, index) {
+                    return Container(
+                      margin: const EdgeInsets.only(right: 5),
+                      padding: const EdgeInsets.all(8),
+                      decoration: BoxDecoration(
+                        color: const Color(0xff00b4d8),
+                        borderRadius: BorderRadius.circular(12),
+                      ),
+                      child: Column(
+                        children: [
+                          "${index + 1} AM".text.gray200.make(),
+                          Image.asset("assets/weather/09n.png"),
+                          "38\u00B0".text.white.make(),
+                        ],
+                      ),
+                    );
+                  },
+                ),
+              ),
+              10.heightBox,
+              const Divider(
+                color: Vx.gray700,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  "Next 7 Days".text.semiBold.size(16).make(),
+                  TextButton(
+                    onPressed: () {},
+                    child: const Text("View All"),
+                  ),
+                ],
+              ),
+              ListView.builder(
+                shrinkWrap: true,
+                itemCount: 7,
+                itemBuilder: (context, index) {
+                  return Card(
+                    child: Container(
+                      padding: const EdgeInsets.symmetric(
+                          horizontal: 8, vertical: 12),
+                      child: Row(
+                        children: const [],
+                      ),
+                    ),
+                  );
+                },
+              ),
+            ],
+          ),
         ),
       ),
     );
